d29fd4c72b68585c0fd2cb525a34e8cc37306900
Minhaj T <65594961+Minhaj-T@users.noreply.github.com>
2025-03-14 10:15:07 +0530

    Time: 11 ms (34.92%) | Memory: 63.1 MB (19.48%) - LeetSync

---
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.ts
@@ -0,0 +1,17 @@
+function reverseVowels(s: string): string {
+    let answer = "";
+    let vowelsIdx = [];
+    const vowels = 'AEIOUaeiou'
+
+    for (let i = 0; i < s.length; i++) {
+        if (vowels.includes(s[i])) vowelsIdx.push(i);
+    }
+
+    for (let i = 0; i < s.length; i++){
+        if(vowels.includes(s[i])) answer += s[vowelsIdx.pop() || 0];
+        else answer +=s[i]
+    }
+
+    return answer;
+
+};
