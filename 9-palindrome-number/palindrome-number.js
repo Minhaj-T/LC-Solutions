b42e699bd77acb26e1f7c2ca6290016c0fb9b8aa
Minhaj T <65594961+Minhaj-T@users.noreply.github.com>
2025-06-10 15:06:06 +0530

    Time: 4 ms (92.80%) | Memory: 64.1 MB (70.35%) - LeetSync

--- 9-palindrome-number/palindrome-number.js
+++ 9-palindrome-number/palindrome-number.js
@@ -3,15 +3,14 @@
  * @return {boolean}
  */
 var isPalindrome = function (x) {
-    if (x < 0) false;
+    if (x < 0) return false;
 
     let rev = 0;
-    
-    let xCopy = x;
+    let xCopy = x
 
     while (x > 0) {
         let rem = x % 10;
-        rev = (10 * rev) + rem
+        rev = rev * 10 + rem
         x = Math.floor(x / 10)
     }
     return rev === xCopy;
