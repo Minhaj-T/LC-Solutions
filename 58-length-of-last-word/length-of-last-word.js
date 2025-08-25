65af57cd36f22cd491849ec2218eca0162dfd924
Minhaj T <65594961+Minhaj-T@users.noreply.github.com>
2025-08-25 12:03:17 +0530

    Time: 0 ms (100.00%) | Memory: 52.2 MB (97.55%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function (s) {
+
+    let n = s.length - 1; // start from last char
+    let count = 0;
+
+    // trim all the last spaces at the end.
+    while (n >= 0) {
+        if (s[n] == " ") {
+            n--;
+        } else {
+            break;
+        }
+    };
+    // n is the word where my last word starts
+
+    // count the carecters till you reach a space;
+    for (let i = n; i >= 0; i--) {
+        if (s[i] === " ") {
+            break;
+        } else {
+            count++;
+        }
+    }
+    return count;
+
+};
