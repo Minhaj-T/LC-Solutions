13c56aae7eff5b6f9a2db90c71db92967d568b68
Minhaj T <65594961+Minhaj-T@users.noreply.github.com>
2025-08-25 14:52:19 +0530

    Time: 0 ms (100.00%) | Memory: 58.6 MB (33.49%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.js
+++ 3194-find-words-containing-character/find-words-containing-character.js
@@ -4,17 +4,12 @@
  * @return {number[]}
  */
 var findWordsContaining = function (words, x) {
-    let out = [];
+    let res = [];
     for (let i = 0; i < words.length; i++) {
-        let char = words[i];
-
-        for (let j = 0; j < char.length; j++) {
-            if (char[j] === x) {
-                out.push(i);
-                break;
-            }
+        if (words[i].includes(x)) {
+            res.push(i);
         }
     }
-    return out;
+    return res;
 
 };
