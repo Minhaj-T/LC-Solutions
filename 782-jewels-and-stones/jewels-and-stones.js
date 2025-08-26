e3b35dd6e30a0b5f6be23e65f225967f434fd6f6
Minhaj T <65594961+Minhaj-T@users.noreply.github.com>
2025-08-26 11:05:54 +0530

    Time: 0 ms (100.00%) | Memory: 53.6 MB (75.70%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.js
+++ 782-jewels-and-stones/jewels-and-stones.js
@@ -4,15 +4,14 @@
  * @return {number}
  */
 var numJewelsInStones = function (jewels, stones) {
-
     let count = 0;
-
     for (let i = 0; i < stones.length; i++) {
-        if (jewels.includes(stones[i])) {
-            count++;
-        }
 
+        for (j = 0; j < jewels.length; j++) {
+            if (jewels[j] == stones[i]) {
+                count++;
+            }
+        }
     }
     return count;
-
 };
