bf9002338b96996b0d23d56efcb43ba67113ec8c
Minhaj T <65594961+Minhaj-T@users.noreply.github.com>
2025-08-27 12:28:53 +0530

    Time: 0 ms (100.00%) | Memory: 53.8 MB (65.82%) - LeetSync

---
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var balancedStringSplit = function (s) {
+    let temp = 0;
+    let count = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        temp = s[i] === "R" ? ++temp : --temp;
+
+        if (!temp) {
+            temp = 0;
+            count++;
+        }
+    }
+    return count;
+
+};
