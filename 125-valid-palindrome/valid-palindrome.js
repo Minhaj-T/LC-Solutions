ef6abc0de6644a3e84a5c49a60e5e1f3d6f0acb0
Minhaj T <65594961+Minhaj-T@users.noreply.github.com>
2025-09-02 11:28:54 +0530

    Time: 7 ms (39.19%) | Memory: 60.5 MB (26.92%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+    let i = 0;
+    let j = s.length - 1;
+    s = s.toLowerCase();
+    while (i < j) {
+        if (!s[i].match(/[a-z0-9]/i)) {
+            ++i;
+        } else if (!s[j].match(/[a-z0-9]/i)) {
+            --j;
+        }
+        else if (s[i] === s[j]) {
+            ++i;
+            --j;
+        } else {
+            return false
+        }
+    }
+    return true;
+
+};
