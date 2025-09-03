62773517b16af31701234186aec263ad0b9f6afe
Minhaj T <65594961+Minhaj-T@users.noreply.github.com>
2025-09-03 11:42:46 +0530

    Time: 12 ms (5.96%) | Memory: 56.7 MB (11.16%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.js
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -6,9 +6,9 @@ var longestCommonPrefix = function (strs) {
     let x = 0;
 
     while (x < strs[0].length) {
-        let char = strs[0][x];
+        let ch = strs[0][x];
         for (let i = 1; i < strs.length; i++) {
-            if (char !== strs[i][x] || x == strs[i].length) {
+            if (ch !== strs[i][x] || x == strs[i].length) {
                 console.log(x, strs[i].length)
                 return strs[0].substring(0, x)
             }
