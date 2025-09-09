d49a1e5dd1d0928ab43b28b1eb6dbfce957b2f9a
Minhaj T <65594961+Minhaj-T@users.noreply.github.com>
2025-09-09 14:30:25 +0530

    Time: 6 ms (68.73%) | Memory: 52.5 MB (99.88%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.js
+++ 205-isomorphic-strings/isomorphic-strings.js
@@ -13,12 +13,9 @@ var isIsomorphic = function (s, t) {
         if (!mapStoT[s[i]] && !mapTtoS[t[i]]) {
             mapStoT[s[i]] = t[i];
             mapTtoS[t[i]] = s[i];
-        } else if (mapTtoS[t[i]] !== s[i]) {
-            return false;
-        } else if (mapStoT[s[i]] !== t[i]) {
+        } else if (mapTtoS[t[i]] !== s[i] || mapStoT[s[i]] !== t[i]) {
             return false;
         }
-
     }
     return true;
 
