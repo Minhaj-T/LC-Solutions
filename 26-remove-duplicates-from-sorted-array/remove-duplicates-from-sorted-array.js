3ac4394cc45af2a787073856233c815c9d0302a3
Minhaj T <65594961+Minhaj-T@users.noreply.github.com>
2025-12-22 15:43:50 +0530

    Time: 0 ms (100.00%) | Memory: 58.4 MB (36.54%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -8,12 +8,11 @@ var removeDuplicates = function (nums) {
 
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] > nums[x]) {
-            nums[x + 1] = nums[i]
-            x++;
+            x = x + 1;
+            nums[x] = nums[i];
         }
 
     }
-
-    return x+1;
+    return x + 1;
 
 };
