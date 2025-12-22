324c66a7e3e6c2e95cb4971cbdb775af501e28d2
Minhaj T <65594961+Minhaj-T@users.noreply.github.com>
2025-12-22 16:37:20 +0530

    Time: 0 ms (100.00%) | Memory: 54 MB (72.53%) - LeetSync

--- 27-remove-element/remove-element.js
+++ 27-remove-element/remove-element.js
@@ -4,15 +4,13 @@
  * @return {number}
  */
 var removeElement = function (nums, val) {
-
     let x = 0;
     for (let i = 0; i < nums.length; i++) {
-
-        if (nums[i] != val) {
+        if (nums[i] !== val) {
             nums[x] = nums[i]
-            x = x + 1
+            x++;
         }
-    }
 
+    }
     return x;
 };
