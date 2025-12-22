6ec7812538fa722664aeb750ce6bb33ede85569d
Minhaj T <65594961+Minhaj-T@users.noreply.github.com>
2025-12-22 15:43:11 +0530

    Time: 0 ms (100.00%) | Memory: 57.6 MB (72.76%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -3,13 +3,17 @@
  * @return {number}
  */
 var removeDuplicates = function (nums) {
+
     let x = 0;
+
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] > nums[x]) {
-            x = x + 1;
-            nums[x] = nums[i]
+            nums[x + 1] = nums[i]
+            x++;
         }
+
     }
-    return x + 1
+
+    return x+1;
 
 };
