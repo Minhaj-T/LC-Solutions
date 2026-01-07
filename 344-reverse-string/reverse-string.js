4d9274dc16620b63b01b3c31bf90f56f86d4e8bc
Minhaj T <65594961+Minhaj-T@users.noreply.github.com>
2026-01-07 11:21:14 +0530

    Time: 1 ms (55.18%) | Memory: 59.8 MB (86.46%) - LeetSync

--- 344-reverse-string/reverse-string.js
+++ 344-reverse-string/reverse-string.js
@@ -4,12 +4,12 @@
  */
 var reverseString = function (s) {
     let len = s.length;
-    let halflen = Math.floor(len / 2)
+    let halfLen = Math.floor(len / 2);
 
-    for (let i = 0; i < halflen; i++) {
+    for (let i = 0; i < halfLen; i++) {
         let temp = s[i];
-        s[i] = s[len - (i + 1)]
-        s[len - (i + 1)] = temp
+        s[i] = s[len - (i + 1)];
+        s[len - (i + 1)] = temp;
     }
-    return s
+    return s;
 };
