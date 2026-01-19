63c85344a79b498c10f06225d514911d38e29626
Minhaj T <65594961+Minhaj-T@users.noreply.github.com>
2026-01-19 14:41:18 +0530

    Time: 170 ms (9.48%) | Memory: 56.7 MB (52.54%) - LeetSync

--- 268-missing-number/missing-number.js
+++ 268-missing-number/missing-number.js
@@ -2,12 +2,25 @@
  * @param {number[]} nums
  * @return {number}
  */
-var missingNumber = function (nums) {
-    let n = nums.length;
-    var totalSum = n * (n + 1) / 2;
-    var partialSum = 0;
-    for (let i = 0; i < n; i++) {
-        partialSum = partialSum + nums[i];
+var missingNumber = function (arr) {
+
+    let n = arr.length;
+
+    for (let i = 0; i <=n; i++) {
+
+        let found = false;
+
+
+        for (let j = 0; j < arr.length; j++) {
+            if (i === arr[j]) {
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            return i;
+        }
+
     }
-    return totalSum - partialSum;
-};
+}
