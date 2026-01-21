adae11b14caa72df862a43250596c7deb04778a1
Minhaj T <65594961+Minhaj-T@users.noreply.github.com>
2026-01-21 12:12:31 +0530

    Time: 1 ms (73.44%) | Memory: 55.3 MB (84.05%) - LeetSync

--- 136-single-number/single-number.js
+++ 136-single-number/single-number.js
@@ -2,21 +2,10 @@
  * @param {number[]} nums
  * @return {number}
  */
-var singleNumber = function (nums) {
-    let hash = {};
-
-    for (let i = 0; i < nums.length; i++) {
-        if (!hash[nums[i]]) {
-            hash[nums[i]] = 1;
-        } else {
-            hash[nums[i]]++
-        }
+var singleNumber = function (arr) {
+    let xor = 0;
+    for (let i = 0; i < arr.length; i++) {
+        xor = xor ^ arr[i];
     }
-
-    for (let i = 0; i < nums.length; i++) {
-        if (hash[nums[i]] === 1) {
-            return nums[i];
-        }
-    }
-
-};
+    return xor;
+}
