28a5f32243512e29c4845aacf785ec5da2dabb57
Minhaj T <65594961+Minhaj-T@users.noreply.github.com>
2026-02-03 16:30:30 +0530

    Time: 62 ms (43.78%) | Memory: 53.9 MB (50.65%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.js
+++ 1013-fibonacci-number/fibonacci-number.js
@@ -5,13 +5,5 @@
 var fib = function(n) {
     if (n <= 1) return n;
 
-    let prev = 0, curr = 1;
-
-    for (let i = 2; i <= n; i++) {
-        let next = prev + curr;
-        prev = curr;
-        curr = next;
-    }
-
-    return curr;
+   return fib(n-1) + fib(n-2)
 };
