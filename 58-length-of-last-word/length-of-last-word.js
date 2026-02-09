262ef168bce623e22044d89335c811387e3dbbbf
Minhaj T <65594961+Minhaj-T@users.noreply.github.com>
2026-02-09 12:05:48 +0530

    Time: 0 ms (100.00%) | Memory: 54.3 MB (18.19%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -3,26 +3,17 @@
  * @return {number}
  */
 var lengthOfLastWord = function (s) {
-
     let n = s.length - 1;
-
-    while (n >= 0) {
-        if (s[n] !== " ") {
-            break;
-        }
-        n--;
-    }
-
     let count = 0;
     for (let i = n; i >= 0; i--) {
 
-        if (s[i] === " ") {
+        if (s[i] !== " ") {
+            count++;
+        } else if (count > 0) {
             break;
         }
-        count++;
 
     }
-
 
     return count;
 
