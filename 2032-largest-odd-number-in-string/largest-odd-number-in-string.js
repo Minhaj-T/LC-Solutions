1188d5d3147e18520cfc7a56038fd14ce6a47939
Minhaj T <65594961+Minhaj-T@users.noreply.github.com>
2026-02-12 11:09:44 +0530

    Time: 3 ms (29.91%) | Memory: 57.9 MB (84.29%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.js
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.js
@@ -3,11 +3,14 @@
  * @return {string}
  */
 var largestOddNumber = function (s) {
-    for (let i = s.length - 1; i >= 0; i--) {
-        if (Number(s[i]) % 2 == 1) {
+
+    for (let i = s.length; i >= 0; i--) {
+
+        if (s[i] % 2 == 1) {
             return s.substring(0, i + 1)
         }
+
     }
-    return "";
+    return ""
 
 };
