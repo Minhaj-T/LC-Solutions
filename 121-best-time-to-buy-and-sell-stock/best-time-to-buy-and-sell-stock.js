2b2f1ade85354339a08cb12a5474e83dbad4ff53
Minhaj T <65594961+Minhaj-T@users.noreply.github.com>
2026-03-10 07:40:18 +0530

    Time: 0 ms (100.00%) | Memory: 62.6 MB (99.90%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -2,18 +2,18 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function (arr) {
-     let min = arr[0];
-    let maxProfit = 0
+var maxProfit = function (prices) {
+    let min = prices[0];
+    let maxProfit = 0;
 
-    for (let i = 1; i < arr.length; i++) {
+    for (let i = 1; i < prices.length; i++) {
 
-        if (maxProfit < (arr[i] - min)) {
-            maxProfit = arr[i] - min;
+        if ((prices[i] - min) > maxProfit) {
+            maxProfit = prices[i] - min;
         }
 
-        if (min > arr[i]) {
-            min = arr[i]
+        if (prices[i] < min) {
+            min = prices[i]
         }
 
     }
