d4cfda12da22af667b6df52522b01ba29d03ec9d
Minhaj T <65594961+Minhaj-T@users.noreply.github.com>
2026-03-18 09:32:08 +0530

    Time: 0 ms (100.00%) | Memory: 54.1 MB (71.95%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.js
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -7,19 +7,24 @@
  */
 var merge = function (nums1, m, nums2, n) {
 
-    let nums1Cpy = nums1.slice(0, m);
-    let p1 = 0, p2 = 0;
+    let cpyNum1 = nums1.slice(0, m);
 
-    for (let i = 0; i < n + m; i++) {
-        if ((nums1Cpy[p1] < nums2[p2] && p1 < m) || p2 >= n) {
-            nums1[i] = nums1Cpy[p1];
+    let p1=0, p2 = 0;
+
+    for (let i = 0; i < m + n; i++) {
+
+        if ((cpyNum1[p1] < nums2[p2] && p1 < m) || p2>=n ) {
+            nums1[i] = cpyNum1[p1];
             p1++;
         } else {
             nums1[i] = nums2[p2];
             p2++;
-
         }
 
+
     }
+
+    return nums1;
+
 
 };
