2a18fe2cb63e146ed86587867e6072519a7a1cb8
Minhaj T <65594961+Minhaj-T@users.noreply.github.com>
2026-05-07 16:29:32 +0530

    Time: 2 ms (86.75%) | Memory: 58 MB (24.78%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.js
+++ 541-reverse-string-ii/reverse-string-ii.js
@@ -3,22 +3,22 @@
  * @param {number} k
  * @return {string}
  */
-var reverseStr = function (s, k) {
-    s = s.split("")
+var reverseStr = function(s, k) { 
+    s = s.split("");
 
-    for (let x = 0; x < s.length; x = x + (2 * k)) {
-        let n = k
-        let mid = Math.floor(n / 2);
+    for(let i=0;i<s.length;i = i+(2*k)){
 
-        for (let i = 0; i < mid; i++) {
-            let temp = s[i + x];
-            s[i + x] = s[x + n - 1 - i];
-            s[x + n - 1 - i] = temp
+        let n=k;
+        let mid = Math.floor(n/2);
+
+        for(let j=0;j<mid;j++){
+            let temp = s[i+j];
+            s[i+j] = s[i+n-(j+1)];
+            s[i+n-(j+1)] = temp;
         }
 
     }
 
-
     return s.join("")
-
+    
 };
