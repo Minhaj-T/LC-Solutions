4d0e6e9e7bf1fb4382f7fb7dba548eeba157adf3
Minhaj T <65594961+Minhaj-T@users.noreply.github.com>
2026-05-11 10:25:10 +0530

    Time: 2 ms (43.20%) | Memory: 54.1 MB (64.18%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.js
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -3,16 +3,19 @@
  * @return {string}
  */
 var longestCommonPrefix = function (strs) {
+
     let x = 0;
 
     while (x < strs[0].length) {
         let ch = strs[0][x];
+
         for (let i = 1; i < strs.length; i++) {
-            if (ch !== strs[i][x] || x == strs[i].length) {
-                return strs[0].substring(0, x)
+            if (strs[i][x] !== ch || x== strs[i].length) {
+                return strs[0].slice(0, x);
             }
         }
-        ++x;
+            ++x;
+
     }
-    return strs[0]
+    return strs[0];
 };
