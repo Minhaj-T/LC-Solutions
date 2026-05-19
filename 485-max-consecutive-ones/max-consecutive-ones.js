442d417b12b16c31a55bc402b3c7f314ffb8d624
Minhaj T <65594961+Minhaj-T@users.noreply.github.com>
2026-05-19 12:58:36 +0530

    Time: 1 ms (92.68%) | Memory: 60.9 MB (14.81%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.js
+++ 485-max-consecutive-ones/max-consecutive-ones.js
@@ -2,23 +2,25 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findMaxConsecutiveOnes = function (nums) {
-
-    let maxCount = 0;
+var findMaxConsecutiveOnes = function (arr) {
     let currCount = 0;
+    let maxCount = 0;
 
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] == 1) {
+    for (let i = 0; i < arr.length; i++) {
+
+        if (arr[i] == 1) {
             currCount++;
-
         } else {
-            if (maxCount < currCount) {
-                maxCount = currCount
+            if (currCount > maxCount) {
+                maxCount = currCount;
             }
             currCount = 0;
         }
+
     }
 
-    return maxCount < currCount ? currCount : maxCount
+    return currCount < maxCount ? maxCount : currCount;
+
+
 
 };
